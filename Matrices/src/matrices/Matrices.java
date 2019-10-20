cade3715884975252534c542858dd837bf3422c5
Luis Alberto Coba Ventura <luis.coba.ventura789@gmail.com>
2019-10-20 15:31:28 -0500

    empezamos a contruir las clases
    construccion de los metodos para la resolucion

--- Matrices/src/matrices/Matrices.java
+++ Matrices/src/matrices/Matrices.java
@@ -9,10 +9,46 @@ import java.util.Scanner;
 
 public class Matrices {
   
-  //
+
   public static void main(String[] args) {
-    // TODO code application logic here
     
+    // este objeto guardara las lines dadas por el usuario
+    Scanner entrada = new Scanner(System.in);
+    
+    operaciones matrizObj = new operaciones();//objeto con constructor vacio
+    operaciones matrizParametros = new operaciones( 1 , 4 );//objeto con constrtor,con parametros int 
+    
+    //Se imprime el mensaje siguiente, indicando al usuario la accion de dar tamaño de matriz 
+    System.out.println( "------Ingresa el tamaño de la matriz------\n" );
+    
+    //solicitamos el numero de filas
+    System.out.println( "Ingresa el numero de filas" );
+    matrizObj.setFilas( entrada.nextInt() );//pasamos el numero capturado con el metodo setFilas a variable filas
+    
+    //solicitamos el numero de columnas 
+    System.out.println( "Ingresa el numero de columnas" );
+    matrizObj.setColumnas( entrada.nextInt() );//pasamoe el numero capturado con el metodo setColumnas a variable filas
+    
+    /*imprimimos un separador*/
+    System.out.println("--------------------------------------------------------");
+    
+    /*declaramos una matriz pasando los valores de tamaño*/
+    int matriz[][] = new int[matrizObj.getFilas()][matrizObj.getColumnas()]; //
+    
+    /*siclos for para el recorrido de la matriz en este caso los usamos para llenarla*/
+    for( int i = 0; i < matrizObj.getFilas(); i++ ){//numero de filas 
+      for( int j = 0; j < matrizObj.getColumnas(); j++ ){//numero de columnas 
+        //mensaje para indcar el lugar del numero 
+        System.out.println( "Ingresa el valor de [" + i + ", " + j + "]"  );
+        //damos el numero de ingresado en la posicion de la matriz dada por el siclo
+        matriz[i][j] = entrada.nextInt();
+      }
+    }
+    //asignamos la nueva matriz en la mtrizObj utilizando el metodo set
+    matrizObj.setMatris(matriz);
+    //se imprime un separador
+    System.out.println( "------------------------------------------"  );
+    matrizObj.solucion();
     
   }//main
 }//class Matrices 
--- Matrices/src/matrices/operaciones.java
+++ Matrices/src/matrices/operaciones.java
@@ -4,13 +4,14 @@
 package matrices;
 
 public class operaciones {
-  int filas = 0, columnas = 0;
+  private int filas; 
+  private int columnas = 0;
   
   int matris[ ][ ] = new int[ filas ][ columnas ];
   
   //constructor vacio
   public operaciones(){ 
-  }
+  }//constructor vacio
   
   //constructor con parametros 
   public operaciones( int filas, int columnas){
@@ -18,35 +19,44 @@ public class operaciones {
     this.filas = filas;
     this.columnas = columnas;
     
-  }
+  }//constructor con parametros 
   
   /*--------------Metodos set-------------------------------*/
   public void setFilas(int filas) {
     this.filas = filas;
-  }
+  }//setFilas
 
   public void setColumnas(int columnas) {
     this.columnas = columnas;
-  }
+  }//setColumnas
 
-  public void setA(int[][] matris) {
+  public void setMatris(int[][] matris) {
     this.matris = matris;
-  }
+  }//setMatris
   
   /*--------------Metodos get-----------------------------*/
 
   public int getFilas() {
     return filas;
-  }
+  }//getFilas
 
   public int getColumnas() {
     return columnas;
-  }
+  }//getColumnas
 
   public int[][] getMatris() {
     return matris;
+  }//getMatris
+  
+  //metodo de solucion 
+  void solucion(){
+    /*For de recorrido de la matris */
+    for( int i = 0; i < filas; i++ ){
+      for(int j = 0; j < columnas; j++){
+        System.out.println( matris[ i ][ j ] );
+      }
+    }
+    
   }
   
-  
-  
-}
+}//class operaciones
